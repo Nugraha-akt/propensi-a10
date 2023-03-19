b7c3013e9c76a8a7be20ee2832d6722a3eb2e067
Juoro Algifari <juoro.algifari@ui.ac.id>
2023-03-19 16:08:55 +0700

    feat/pengelolaan-driver

---
+++ src/main/java/apap/propensi/mantra/contoller/DriverController.java
@@ -0,0 +1,151 @@
+package apap.propensi.mantra.contoller;
+
+import apap.propensi.mantra.model.DriverModel;
+import apap.propensi.mantra.model.RequestModel;
+import apap.propensi.mantra.service.DriverService;
+import apap.propensi.mantra.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/driver")
+public class DriverController {
+
+    @Autowired
+    private DriverService driverService;
+
+    @Autowired
+    private RequestService requestService;
+
+    @GetMapping("/viewall")
+    public String listDriverOrderByStatus(Model model) {
+        List<DriverModel> listDriverOrderByStatus = driverService.getListDriverOrderByStatus();
+        model.addAttribute("listDriver", listDriverOrderByStatus);
+        return "driver/viewall";
+    }
+
+    @GetMapping("/detail/{uuid}")
+    public String detailDriver(@PathVariable String uuid, Model model){
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        if (driver != null){
+            List<RequestModel> listRequest = driver.getListRequest();
+            model.addAttribute("driver", driver);
+            model.addAttribute("listRequest", listRequest);
+            return "driver/detail";
+        }
+        return "driver/404";
+    }
+
+    @GetMapping("/nonaktifkan/{uuid}")
+    public String nonaktifkanDriver(@PathVariable String uuid, Model model){
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        if (driver != null){
+            model.addAttribute("driver", driver);
+            return "driver/validasi-nonaktifkan";
+        }
+        return "driver/404";
+    }
+
+    @PostMapping("/nonaktifkan-berhasil/{uuid}")
+    public String nonaktifkanBerhasilDriver(@PathVariable String uuid, Model model) {
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        driver.setStatus(4);
+        DriverModel nonaktifDriver = driverService.updateDriver(driver);
+        model.addAttribute("driver", nonaktifDriver);
+        return "driver/nonaktifkan";
+    }
+
+    @GetMapping("/aktifkan/{uuid}")
+    public String aktifkanDriver(@PathVariable String uuid, Model model){
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        if (driver != null){
+            model.addAttribute("driver", driver);
+            return "driver/validasi-aktifkan";
+        }
+        return "driver/404";
+    }
+
+    @PostMapping("/aktifkan-berhasil/{uuid}")
+    public String aktifkanBerhasilDriver(@PathVariable String uuid, Model model) {
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        driver.setStatus(2);
+        DriverModel aktifDriver = driverService.updateDriver(driver);
+        model.addAttribute("driver", aktifDriver);
+        return "driver/aktifkan";
+    }
+
+    @GetMapping("/ringkasan")
+    public String ringkasanDriver(Model model) {
+        Integer sumSedangDitugaskan = driverService.getSumSedangDitugaskan();
+        Integer sumBelumDitugaskan = driverService.getSumBelumDitugaskan();
+        Integer sumSudahDitugaskan = driverService.getSumSudahDitugaskan();
+        Integer sumTidakAktif = driverService.getSumTidakAktif();
+        model.addAttribute("sumSedangDitugaskan", sumSedangDitugaskan);
+        model.addAttribute("sumBelumDitugaskan", sumBelumDitugaskan);
+        model.addAttribute("sumSudahDitugaskan", sumSudahDitugaskan);
+        model.addAttribute("sumTidakAktif", sumTidakAktif);
+        return "driver/ringkasan";
+    }
+
+    @GetMapping("/update/{uuid}")
+    public String updateDriver(@PathVariable String uuid, Model model){
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        if (driver != null){
+            if (driver.getStatus()==2 || driver.getStatus()==3) {
+                List<RequestModel> listRequestMulai = requestService.getListRequestMulai();
+                model.addAttribute("listRequest", listRequestMulai);
+                model.addAttribute("driver", driver);
+                return "driver/form-update";
+            } else if (driver.getStatus()==1) {
+                String statusPerjalanan = requestService.getRequestByDriverUuid(uuid).getStatus();
+                if (!statusPerjalanan.equals("SAMPAI")){
+                    model.addAttribute("driver", driver);
+                    return "driver/belum-sampai";
+                }
+                else {
+                    model.addAttribute("driver", driver);
+                    return "driver/validasi-sampai";
+                }
+            }
+        }
+        return "driver/404";
+    }
+
+    @GetMapping("/update-tugaskan/{uuid}/{id}")
+    public String updateSedangDitugaskan(@PathVariable String uuid, @PathVariable Long id, Model model){
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        RequestModel request = requestService.getRequestById(id);
+        if (driver != null){
+            model.addAttribute("driver", driver);
+            model.addAttribute("request", request);
+            return "driver/validasi-tugaskan";
+        }
+        return "driver/404";
+    }
+
+    @PostMapping("/update-sedang/{uuid}/{id}")
+    public String updateBerhasilDriver(@PathVariable String uuid,@PathVariable Long id, Model model) {
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        RequestModel request = requestService.getRequestById(id);
+        driver.setStatus(1);
+        request.setDriver(driver);
+        DriverModel updateDriver = driverService.updateDriver(driver);
+        RequestModel updateRequest = requestService.updateRequest(request);
+        model.addAttribute("driver", updateDriver);
+        model.addAttribute("request", updateRequest);
+        return "driver/update";
+    }
+
+    @PostMapping("/update-sudah/{uuid}")
+    public String updateSudahDitugaskanDriver(@PathVariable String uuid, Model model) {
+        DriverModel driver = driverService.getDriverByUuid(uuid);
+        driver.setStatus(3);
+        DriverModel sudahDitugaskanDriver = driverService.updateDriver(driver);
+        model.addAttribute("driver", sudahDitugaskanDriver);
+        return "driver/update";
+    }
+}
---
+++ src/main/java/apap/propensi/mantra/service/RequestServiceImpl.java
@@ -0,0 +1,38 @@
+package apap.propensi.mantra.service;
+
+import apap.propensi.mantra.model.RequestModel;
+import apap.propensi.mantra.repository.RequestDb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class RequestServiceImpl implements RequestService{
+
+    @Autowired
+    private RequestDb requestDb;
+
+    @Override
+    public List<RequestModel> getListRequestMulai() {
+        return requestDb.listRequestMulai();
+    }
+
+    @Override
+    public RequestModel getRequestByDriverUuid(String uuid) {
+        return requestDb.findByDriverUuid(uuid);
+    }
+
+    @Override
+    public RequestModel getRequestById(Long id) {
+        return requestDb.findById(id);
+    }
+
+    @Override
+    public RequestModel updateRequest(RequestModel request) {
+        requestDb.save(request);
+        return request;
+    }
+}
