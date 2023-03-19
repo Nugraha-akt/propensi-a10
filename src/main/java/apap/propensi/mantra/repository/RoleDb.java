ab99f5044a6006c5bab89d73aa07e898facf62a7
Nugraha Akt <nugraha.aktwovhiday@ui.ac.id>
2023-03-19 11:58:55 +0700

    add profile and blank

--- src/main/java/apap/propensi/mantra/repository/RoleDb.java
+++ src/main/java/apap/propensi/mantra/repository/RoleDb.java
@@ -1,15 +1,15 @@
-package apap.propensi.mantra.repository;
-
-import apap.propensi.mantra.model.RoleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.config.web.servlet.oauth2.resourceserver.OpaqueTokenDsl;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-
-@Repository
-public interface RoleDb extends JpaRepository<RoleModel, Long> {
-
-    Optional<RoleModel> findById(Long id);
-}
+//package apap.propensi.mantra.repository;
+//
+//import apap.propensi.mantra.model.RoleModel;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.security.config.web.servlet.oauth2.resourceserver.OpaqueTokenDsl;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//
+//@Repository
+//public interface RoleDb extends JpaRepository<RoleModel, Long> {
+//
+//    Optional<RoleModel> findById(Long id);
+//}
