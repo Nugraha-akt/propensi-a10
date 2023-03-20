19787cd3c25ce17c95debd3f6019bdf2716fd9b7
Nugraha Aktwovhiday Abdullah G <nugraha.aktwovhiday@ui.ac.id>
2023-03-20 11:04:34 +0700

    add sec:auth in template and change login design

--- src/main/java/apap/propensi/mantra/service/DriverService.java
+++ src/main/java/apap/propensi/mantra/service/DriverService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface DriverService {
     List<DriverModel> getListDriverOrderByStatus();
     DriverModel getDriverByUuid(String uuid);
+    DriverModel getDriverByUsername(String username);
     DriverModel updateDriver(DriverModel driver);
     Integer getSumSedangDitugaskan();
     Integer getSumBelumDitugaskan();
